d74081396eca200d94a51710ddcd6742c9d53524
mxs3419 <mxs3419@g.rit.edu>
2018-03-13 16:57:01 -0400

    Initial Commit

---
+++ AuthProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AuthProject
+//
+//  Created by Michael Schade on 3/13/18.
+//  Copyright © 2018 Michael Schade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
